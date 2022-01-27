f491b5936126a361d162f5bd24db97317733d437
André van Schoubroeck <andre@blaatschaap.be>
2022-01-27 13:33:17 +0100

    bshal nrfx i2c fixes

--- bshal/nrfx/bshal_i2cm_nrfx.c
+++ bshal/nrfx/bshal_i2cm_nrfx.c
@@ -16,11 +16,20 @@
 #include "nrfx.h"
 #include "nrfx_twim.h"
 
-bool m_busy = false;
+
+#define NONBLOCKINGAPI
+
+
+#if defined NONBLOCKINGAPI
+static volatile bool m_busy = false;
 static void nrfx_twim_evt_handler(nrfx_twim_evt_t const *p_event,
 		void *p_context) {
 	m_busy = false;
+
+	if (p_event->type != NRFX_TWIM_EVT_DONE)
+		__BKPT(0);
 }
+#endif
 
 int bshal_nrfx_i2cm_send(void *drv_specific, uint8_t address, void *p_data,
 		uint8_t length, bool nostop) {
@@ -29,17 +38,24 @@ int bshal_nrfx_i2cm_send(void *drv_specific, uint8_t address, void *p_data,
 	xfer.address = address;
 	xfer.p_primary_buf = p_data;
 	xfer.p_secondary_buf = NULL;
-	xfer.primary_length = p_data;
+	xfer.primary_length = length;
 	xfer.secondary_length = 0;
 	xfer.type = NRFX_TWIM_XFER_TX;
 
-	int status = nrfx_twim_xfer(drv_specific, &xfer,
+#if defined NONBLOCKINGAPI
+	m_busy = true;
+#endif
+
+	int16_t status = nrfx_twim_xfer(drv_specific, &xfer,
 			nostop ? NRFX_TWIM_FLAG_TX_NO_STOP : 0);
 	if (status)
 		return I2C_ERROR;
 	// TODO We need to be able to escape on hang
-	while (m_busy)
-		;
+#if defined NONBLOCKINGAPI
+	// TODO We need to be able to escape on hang
+	while (m_busy);
+#endif
+	return I2C_OK;
 }
 int bshal_nrfx_i2cm_recv(void *drv_specific, uint8_t address, void *p_data,
 		uint8_t length, bool nostop) {
@@ -52,14 +68,19 @@ int bshal_nrfx_i2cm_recv(void *drv_specific, uint8_t address, void *p_data,
 	xfer.secondary_length = 0;
 	xfer.type = NRFX_TWIM_XFER_RX;
 
+#if defined NONBLOCKINGAPI
 	m_busy = true;
+#endif
+
 	int16_t status = nrfx_twim_xfer(drv_specific, &xfer, 0);
 	if (status)
 		return I2C_ERROR;
-	// TODO We need to be able to escape on hang
-	while (m_busy)
-		;
 
+#if defined NONBLOCKINGAPI
+	// TODO We need to be able to escape on hang
+	while (m_busy);
+#endif
+	return I2C_OK;
 }
 
 int bshal_nrfx_i2cm_isok(void *drv_specific, uint8_t address) {
@@ -78,8 +99,14 @@ int bshal_nrfx_i2cm_init(bshal_i2cm_instance_t *i2c_instance) {
 	config.sda = i2c_instance->sda_pin;
 	config.frequency = NRF_TWIM_FREQ_400K;
 
-	nrfx_twim_init(i2c_instance->drv_specific, &config, nrfx_twim_evt_handler,
-			NULL);
+	nrfx_twim_init(i2c_instance->drv_specific, &config,
+#if defined NONBLOCKINGAPI
+			nrfx_twim_evt_handler,
+#else
+			NULL,
+#endif
+			i2c_instance);
 	nrfx_twim_enable(i2c_instance->drv_specific);
+	return I2C_OK;
 
 }
---
+++ bshal/nrfx/bshal_i2cm_nrfx.h
@@ -0,0 +1,14 @@
+/*
+ * bshal_i2cm_nrfx.h
+ *
+ *  Created on: 27 jan. 2022
+ *      Author: andre
+ */
+
+#ifndef UCDEV_LIB_LIBHALGLUE_BSHAL_NRFX_BSHAL_I2CM_NRFX_H_
+#define UCDEV_LIB_LIBHALGLUE_BSHAL_NRFX_BSHAL_I2CM_NRFX_H_
+
+int bshal_nrfx_i2cm_init(bshal_i2cm_instance_t *i2c_instance);
+
+
+#endif /* UCDEV_LIB_LIBHALGLUE_BSHAL_NRFX_BSHAL_I2CM_NRFX_H_ */
