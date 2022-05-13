e01d0e5d27fee28e78945434cd98b4c26adabeda
André van Schoubroeck <andre@blaatschaap.be>
2022-05-13 12:38:35 +0200

    GD32VF103/CH32V103 compatibility layer for STM32F103 HAL

---
+++ compat/rv32f103/ch32.c
@@ -0,0 +1,8 @@
+/*
+ * ch32.c
+ *
+ *  Created on: 7 mei 2022
+ *      Author: andre
+ */
+
+// No C init for CH32 (for now)
--- hal/hal.h
+++ hal/hal.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-
 #if defined (NRF51) ||\
     defined (NRF51422_XXAA) ||\
     defined (NRF51422_XXAB) ||\
