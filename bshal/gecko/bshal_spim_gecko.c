1d5b7e49fb33cf2ac5fb1b37e749d6221e26531b
U-DESKTOP-TR68E3Q\andre <Andr van Schoubroeck (Lenovo laptop)>
2023-05-15 15:55:46 +0200

    SPI

--- bshal/gecko/bshal_spim_gecko.c
+++ bshal/gecko/bshal_spim_gecko.c
@@ -5,6 +5,58 @@
  *      Author: andre
  */
 
+#include <bshal_spim.h>
+
+#include <em_device.h>
 
 
+int bshal_spim_transmit(bshal_spim_instance_t *spim_instance, void *data,
+		size_t amount, bool nostop) {
+	GPIO_PORT_TypeDef port;
+	uint8_t pin;
 
+	bshal_gpio_write_pin(spim_instance->cs_pin, spim_instance->cs_pol);
+	uint8_t *data8 = data;
+
+	for (int i = 0; i < sizeof(amount); i++)
+		USART_SpiTransfer(spim_instance->drv_specific, data8[i]);
+
+	if (!nostop)
+		bshal_gpio_write_pin(spim_instance->cs_pin, !spim_instance->cs_pol);
+
+	return 0;
+}
+
+int bshal_spim_receive(bshal_spim_instance_t *spim_instance, void *data,
+		size_t amount, bool nostop) {
+	GPIO_PORT_TypeDef port;
+	uint8_t pin;
+
+	bshal_gpio_write_pin(spim_instance->cs_pin, spim_instance->cs_pol);
+	uint8_t *data8 = data;
+
+	for (int i = 0; i < sizeof(amount); i++)
+		data8[i] = USART_SpiTransfer(spim_instance->drv_specific, 0xFF);
+
+	if (!nostop)
+		bshal_gpio_write_pin(spim_instance->cs_pin, !spim_instance->cs_pol);
+
+	return 0;
+}
+
+int bshal_spim_transceive(bshal_spim_instance_t *spim_instance, void *data,
+		size_t amount, bool nostop) {
+	GPIO_PORT_TypeDef port;
+	uint8_t pin;
+
+	bshal_gpio_write_pin(spim_instance->cs_pin, spim_instance->cs_pol);
+	uint8_t *data8 = data;
+
+	for (int i = 0; i < sizeof(amount); i++)
+		data8[i] = USART_SpiTransfer(spim_instance->drv_specific, data8[i]);
+
+	if (!nostop)
+		bshal_gpio_write_pin(spim_instance->cs_pin, !spim_instance->cs_pol);
+
+	return 0;
+}
